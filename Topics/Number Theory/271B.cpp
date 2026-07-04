14cd66a960cff1dd82a86958b82103deb7602997
Sadeel-Qashoa <sadeelqashoa0@gmail.com>
2026-07-04 14:10:34 +0300

    Solve Codeforces 271B

--- Topics/Number Theory/271B.cpp
+++ Topics/Number Theory/271B.cpp
@@ -1,3 +1,74 @@
-//
-// Created by Qashoo on 04/07/2026.
-//
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+const ll N = 1e7;
+// The Great Eng : Sadeel Qashoa
+bool is_prime[N];
+vector<int> primes;
+
+void seive()
+{
+    // O(n)
+    fill(is_prime + 1, is_prime + N, true);
+    is_prime[0] = is_prime[1] = false;
+    for (ll i = 2; i < N; i++)
+    {
+        if (is_prime[i])
+        {
+            primes.push_back(i);
+            for (ll j = i * i; j < N; j += i)
+            {
+                is_prime[j] = false;
+            }
+        }
+    }
+}
+
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+    seive();
+    int a[n][m];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> a[i][j];
+            auto it = lower_bound(primes.begin(), primes.end(), a[i][j]);
+            a[i][j] = *it - a[i][j];
+        }
+    }
+    int sum = 0, mn = INT_MAX;
+    for (int i = 0; i < n; i++)
+    {
+        sum=0;
+        for (int j = 0; j < m; j++)
+        {
+            sum += a[i][j];
+        }
+        mn = min(mn, sum);
+    }
+    for (int i = 0; i < m; i++)
+    {
+        sum = 0;
+        for (int j = 0; j < n; j++)
+        {
+            sum += a[j][i];
+        }
+        mn = min(mn, sum);
+    }
+    cout << mn;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t=1;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
--- Topics/Number Theory/prime.cpp
+++ Topics/Number Theory/prime.cpp
@@ -1,41 +1,48 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
+const ll N = 1e7;
 // The Great Eng : Sadeel Qashoa
-void solve()
-{
-
-}
 bool neivePrime()
 {
-    int n; cin>>n;
+    //O(n)
+    int n;
+    cin >> n;
     int cnt = 0;
-    for(int i=1; i<=n; i++)
+    for (int i = 1; i <= n; i++)
     {
-        if(n%i==0)
+        if (n % i == 0)
         {
             cnt++;
         }
     }
-    if(cnt == 2) return true;
+    if (cnt == 2) return true;
     else return false;
 }
+
 bool prime(int n)
 {
-    if(n==1) return false;
-    if(n==2) return true;
-    for (int i=3 ; i * i<=n ; i++)
+    //O(sqrt(n/2))
+    if (n == 1) return false;
+    if (n == 2) return true;
+    for (int i = 3; i * i <= n; i += 2)
     {
-        if(n % i == 0)
+        if (n % i == 0)
             return false;
     }
     return true;
 }
+void solve()
+{
+
+}
+
 int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    int t ; cin >> t;
+    int t;
+    cin >> t;
     while (t--)
     {
         solve();
